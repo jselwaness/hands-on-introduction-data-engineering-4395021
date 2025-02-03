384123d350a4388bd3e90f89fbc5809a3a8c72d4
jselwaness <77246279+jselwaness@users.noreply.github.com>
2025-02-03 15:16:52 +0000

    Added one task dag file

---
+++ airflow/dags/one_task_dag.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from airflow.operators.bash import BashOperator
+from airflow import DAG
+
+default_args = {
+        'owner': 'James',
+        'depends_on_past': False,
+        'email_on_failure': True,
+        'email_on_retry': True,
+        'retries': 0,
+        'catchup': False,
+        'startdate': datetime(2023,1,1)
+}
+
+with DAG(
+        dag_id = 'one_task_dag',
+        description='A one task Airflow DAG',
+        schedule_interval=None,
+        default_args=default_args
+) as dag:
+    
+    task1 = BashOperator(
+        task_id = 'one_task',
+        bash_command='echo "hello, linkedin learning!" > /workspaces/hands-on-introduction-data-engineering-4395021/'
+    )
